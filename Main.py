78e80af39f9707d97f946ac6df73cb1ebe8d5506
diego-reb <dzgarcia10@gmail.com>
2025-03-31 23:41:52 -0600

    vistas finalizadas

--- Main.py
+++ Main.py
@@ -6,6 +6,19 @@ app = Flask(__name__)
 def Main():
     return render_template('index.html')
 
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/registro")
+def registro():
+    return render_template('registro.html')
+
+@app.route('/menu')
+def registro():
+    return render_template('menu.html')
+
+
 if __name__=='__main__':
     app.run(debug=True)
 
